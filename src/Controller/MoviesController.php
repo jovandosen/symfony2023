06ff3b6c22c7276e5a525e1ffea3efee595113c4
Jovan Dosen <jovandosen1994@gmail.com>
2023-06-05 09:52:20 +0200

    Few Movie entity queries added

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,10 +12,28 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
-        $movies = ["Terminator", "Blade", "Loki", "Lord of the rings", "Hobbit"];
+        // findAll() -> SELECT * FROM movies;
+        // find() -> SELECT * FROM movies WHERE id = 5;
+        // findBy() -> SELECT * FROM movies ORDER BY id DESC;
+        // findOneBy() -> SELECT * FROM movies WHERE id = 5 AND releaseYear = 2008 ORDER BY id DESC
+        // count() -> SELECT COUNT(id) FROM movie;
+        // getClassName() -> App\Entity\Movie
+
+        $repository = $this->em->getRepository(Movie::class);
+
+        $movies = $repository->getClassName();
+
+        dd($movies);
 
         return $this->render('index.html.twig', [
             'title' => 'List of movies',
